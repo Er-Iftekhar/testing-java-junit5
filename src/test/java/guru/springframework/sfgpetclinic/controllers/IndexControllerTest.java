f4f77584cec108abdb1a88ad59c9732df6c0302a
SyedIftekharuddin <syedy636@gmail.com>
2020-12-31 10:08:45 +0100

    @EnableXXX methods, AssertJ dependency added and some Assumptions also added.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+@DisplayName("Test for IndexController")
 class IndexControllerTest {
 
     IndexController indexController;
@@ -14,6 +19,7 @@ class IndexControllerTest {
         indexController = new IndexController();
     }
 
+    @DisplayName("Test Proper view name is returned for index page")
     @Test
     void index() {
         assertEquals("index", indexController.index());
@@ -21,13 +27,84 @@ class IndexControllerTest {
         assertEquals("index", indexController.index(), ()-> "Some  expensive method" +
                 "For display a failed execution");
 
+        assertThat(indexController.index()).isEqualTo("index");
+
     }
 
     @Test
-    void oupsHandler() {
-        assertTrue("notimplemented".equals(indexController.oupsHandler()));
-        assertEquals("notimplemented", indexController.oupsHandler(), "Not so expensive method");
-        assertEquals("notimplemented", indexController.oupsHandler(), ()-> "Some  expensive method" +
-                "For display a failed execution");
+    @DisplayName("Test exception")
+    void oopsHandler() {
+
+        assertThrows(ValueNotFoundException.class, ()-> {
+            indexController.oopsHandler();
+        });
+
+        /*Before implementing the exception throwing test
+        assertTrue("notimplemented".equals(indexController.oopsHandler()));
+        assertEquals("notimplemented", indexController.oopsHandler(), "Not so expensive method");
+        assertEquals("notimplemented", indexController.oopsHandler(), ()-> "Some  expensive method" +
+                "For display a failed execution");*/
+    }
+
+    @Disabled("Demo for timeout")
+    @Test
+    public void whenThrowsExceptionAfterTimeout_thenCorrect(){
+        assertTimeout(Duration.ofMillis(200),
+                ()-> Thread.sleep(2000));
+    }
+
+    @Disabled("Demo for timeout preemptively")
+    @Test
+    public void testTimeoutPreemptively(){
+        assertTimeoutPreemptively(Duration.ofMillis(200),
+                ()-> Thread.sleep(5));
+    }
+
+
+    @Test
+    public void testAssumption(){
+        assumeTrue("Yasar".equalsIgnoreCase(System.getenv("YASAR_RUNTIME")));
+    }
+
+    @Test
+    public  void testAssumptionsWhenTrue(){
+        assumeTrue("Yasar".equalsIgnoreCase("Yasar"));
+    }
+
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    public void testOnWindows(){
+
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    public void testOnLinux(){
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    public void testOnJava8(){
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_11)
+    public void testOnJava11(){
+
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "USERNAME", matches = "syedy")
+    public void testIfUserSyedy(){
+
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "USERNAME", matches = "guru")
+    public void testIfUserGuru(){
+
     }
 }
