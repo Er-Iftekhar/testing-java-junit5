5cdea3bdb5541fd57a6df454a6f7822f2d774fc1
SyedIftekharuddin <syedy636@gmail.com>
2021-01-02 08:53:03 +0100

    CSV tests with both file and list of values.

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,4 +34,56 @@ class OwnerTest implements ModelTests {
 
         assertThat(owner.getCity(), is("Prague"));
     }
+
+    @DisplayName("value source test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ValueSource(strings = {"Spring", "Framework", "Guru"})
+    void testValueSource(String val){
+        System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name ="{displayName} - [{index}] {arguments}" )
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "IND, 1, 1",
+            "AUS, 2, 1",
+            "QAT, 1, 3",
+    })
+    void csvInputTest(String countryName, int val1, int val2){
+        System.out.println(countryName + " - " + val1 + " : " + val2);
+    }
+
+
+    @DisplayName("CSV from file")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/csvtest.csv", numLinesToSkip = 1)
+    void testCsvFromFile(String countryName, int val1, int val2){
+        System.out.println(countryName + " - " + val1 + " : " + val2);
+    }
+
+
+
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void testMethodArgs(String countryName, int val1, int val2){
+        System.out.println(countryName + " - " + val1 + " : " + val2);
+    }
+
+    static Stream<Arguments> getArgs(){
+        return Stream.of(
+                Arguments.of("IND", 3, 1),
+                Arguments.of("AUS", 1, 6),
+                Arguments.of("PAK", 5, 1)
+        );
+    }
+
+
 }
